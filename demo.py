8ef1b29508a4baa954abc573ea14d77112c694a4
kevinjoses <101249625+kevinjoses@users.noreply.github.com>
2024-10-14 16:31:38 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,260 @@
+# print('NAME:kevin jose')
+# print(" PHONE NUMBER:9562640213")
+# firstname=input("enter first name:")
+# lastname=input("enter last name:")
+# age=input("enter your age:")
+# phonenumber=input("enter your phone number:")
+# Gender=input("enter your gender:")
+# print("_________________")
+# print("your name:",firstname,lastname)
+# print("your age is :",age)
+# print("your phone number:",phonenumber)
+# print("your gender:",Gender)
+
+# x=10
+# y=8
+# print(x+y)
+
+# x=3
+# y=2
+# print(x-y)
+
+# x=20
+# y=10
+# print(x*y)
+
+# x=25
+# y=6
+# print(x/y)
+
+# x=90
+# y=6
+# print(x%y)
+
+# x=70
+# y=10
+# print(x**y)
+
+# x=50
+# y=40
+# print(x//y)
+
+# x=55
+# x+=6
+# print(x)
+
+# x=55
+# x-=6
+# print(x)
+
+# x=70
+# x*=6
+# print(x)
+
+# x=100
+# x/=50
+# print(x)
+
+# x=150
+# x%=50
+# print(x)
+
+# x=150
+# x//=50
+# print(x)
+
+# x=150
+# x**=50
+# print(x)
+
+# a=90
+# b=90
+# if a==b:
+#     print(a," is equal to ",b)
+#     print("helo world")
+# else:
+#     print(a," is not equal to ",b)
+
+
+# name=input("enter your name;")
+# age=int(input("enter your age;"))
+# if age>18:
+#     print("hello",name,"congrats your are eligible for voting")
+# else:
+#     print("hello",name,"Sorry your not eligible for voting ")
+
+# number1=int(input("enter the first number;"))
+# number2=int(input("enter the second number;"))
+# if number1 > number2:
+#     print(number1,"is greater ;")
+# else:
+#     print(number2,"is greater;")
+    
+
+
+# number1=int(input("enter the first number;"))
+# if number1>0:
+#     print("its a +ve number")
+# elif number1<0:
+#     print("-ve number")
+# elif number1==0:
+#     print("its a zero")
+# else:
+#     print("invalid input")
+
+
+# number1=int(input("enter the first number;"))
+
+# if ((number1%2==0) and (number1!=0)):
+#     print("even")
+# elif number1%2!=0:
+#     print("odd")
+# else:
+#     print("its a zerro")
+
+# number1=int(input("enter the first number;"))
+# if((number1%2==0)and (number1!=0)):
+#     print("even")
+# elif number1%2!=0:
+#     print("odd")
+# else:
+#     print("its a zero")
+
+
+
+
+
+# if condition:
+#     if condition:
+#         if condition:
+        
+#         elif condition:
+
+#         else:
+#             if condition:
+#                 if  condition:
+
+
+
+
+# elif condition:
+
+
+
+
+# a=int(input("enter a start value :"))
+# b=int(input("enter a stop value :"))
+
+# while a<=b:
+#     print(a)
+#     a+=1
+
+# a=int(input("enter a start value :"))
+# b=int(input("enter a stop value :"))
+
+# while a<=b:
+#     print(a)
+#     a+=5
+
+
+# a=int(input("enter the start value:"))
+# b=int(input("enter the end value:"))
+
+# while a<=b:
+#     print(a)
+#     a+=150
+
+
+
+
+
+
+# i=5
+# for i in range(10):
+#     print(i)
+
+# for(i=0;i<10;i+2)
+# for v in range(1,11):
+#     print(v,"x",i,"=",i*v)
+
+
+    
+# j=int(input("enter the number:"))
+# i=1
+# while(i<=10):
+#     print(i,"x",j,"=",i*j)
+#     i=i+1
+
+
+# for i in range(10):
+#     print(i)
+# for v in range(10,21,5):
+#     print(v,"\n",v+100)
+    
+
+a=int(input("enter a number:"))
+i=1
+temp=0
+while(i<=a):
+    temp=temp+i
+    i+=1
+print("product of",a,"natural number is",temp)
+
+smaller=int(input("enter the smaller number:"))
+larger=int(input("enter the larger number"))
+# i=smaller
+temp=1
+while smaller<=larger:
+    temp=temp*smaller  
+    smaller=smaller+1
+
+
+print(temp)
+
+
+# smaller=int(input("enter the smaller number:"))
+# larger=int(input("enter the larger number"))
+# temp=1
+# while smaller<=larger:
+#     if smaller%2==0:
+#         temp=temp*smaller 
+#     smaller=smaller+1
+
+# print(temp)
+
+# smaller=int(input("enter the smaller limit:"))
+# larger=int(input("enter the larger limit:"))
+# temp=0
+# while smaller<=larger:
+#     if smaller%5==0 and smaller%7==0:
+#         temp=temp+smaller
+#         print(smaller)
+#     smaller+=1
+# print(temp)
+
+
+
+# i = 3
+# while i < 7:
+#   print (i)
+#   if (i==1):
+#     break
+#   i -= 1
+
+
+
+
+# number=int("input the number:")
+# even=0
+# odd=0
+# temp=0
+# while(number>0):
+#     temp=number%10
+
+    
+
+
+
+
+
+
