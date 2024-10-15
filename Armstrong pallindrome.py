787570fb482cafa81ba591a5a3eb2ac24f7b4b11
kevin jose <101249625+kevinjoses@users.noreply.github.com>
2024-10-15 16:26:04 +0530

    Add files via upload

---
+++ Armstrong pallindrome.py
@@ -0,0 +1,12 @@
+num=1634
+order=len(str(num))
+sum=0
+temp=num
+while num>0:
+    r=temp%10
+    sum+=r**order
+    temp//=10
+if num==sum:
+    print(num,"is an armstrong number")
+else:
+    print(num,"is not an armstrong number")
