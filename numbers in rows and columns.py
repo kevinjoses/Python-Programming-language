f2ae55c340bc7a96aafddadcee272f82a152a2f7
kevin jose <101249625+kevinjoses@users.noreply.github.com>
2024-10-18 16:36:15 +0530

    Add files via upload

---
+++ numbers in rows and columns.py
@@ -0,0 +1,55 @@
+
+    
+# num=int(input("enter the value:"))
+
+# for i in range(1,num+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+
+
+# num=int(input("enter the value:"))
+# for i in range(1,num+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+    
+
+
+# num=int(input("enter the value:"))
+# for i in range(1,num+1):
+#         for j in range(1,i+1):
+#             print(j,end=" ")
+#         print()
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4 
+# 1 2 3 4 5
+
+
+num=int(input("enter the value:"))
+for i in range(5):
+    print(1,end=" ")
+print()
+for i in range(4):
+    print(2,end=" ")
+print()
+for i in range(3):
+    print(3,end=" ")
+print()
+for i in range(2):
+    print(4,end=" ")
+print()
+for i in range(1):
+    print(5,end=" ")
+print()
+
+
+# 1 1 1 1 1 
+# 2 2 2 2
+# 3 3 3
+# 4 4
+# 5
