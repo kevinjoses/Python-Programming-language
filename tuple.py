f805ebd722d67dde98994eb6e70f22aee8eaf8a3
kevin jose <101249625+kevinjoses@users.noreply.github.com>
2024-11-04 17:09:18 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,29 @@
+# unpacking a tuple
+
+# fruits=("apple","banana","cherry")
+# (green,yellow,red)=fruits
+# print(green)
+# print(yellow)
+# print(red)
+
+# asterisk 
+
+# fruits=("apple","banana","cherry")
+# (green,yellow,*red)=fruits
+# print(green)
+# print(yellow)
+# print(red)
+
+# using while loop in tuple
+
+# thistuple=("apple","banana","strawberry")
+# i=0
+# while i<len(thistuple):
+#     print(thistuple[i])
+#     i=i+1
+
+# using for loop in tuple
+    
+thistuple=("apple","banana","strawberry")
+for i in range(len(thistuple)):
+    print(thistuple[i])
