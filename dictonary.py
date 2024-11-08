1d5d1d1050d4b8460d7e49563ddbe058d093b312
kevin jose <101249625+kevinjoses@users.noreply.github.com>
2024-11-08 16:50:29 +0530

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,9 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if 1964 in thisdict.values():
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+else: 
+   print("no")
