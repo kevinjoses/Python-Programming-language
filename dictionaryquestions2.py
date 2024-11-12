a4dd22df50bf6a1c3fb06ff04d410268c486f30b
kevin jose <101249625+kevinjoses@users.noreply.github.com>
2024-11-12 16:01:23 +0530

    Add files via upload

---
+++ dictionaryquestions2.py
@@ -0,0 +1,10 @@
+# Q1
+# write a python program to sum all the the items in a dictionary.
+
+me={"a":6,"b":7,"c":8}
+kevin=multiply(me.values())
+print(f"sum of all the dictionary:{kevin}")
+
+# Q2
+# write a python program to multiply all the items in a dictionary
+
