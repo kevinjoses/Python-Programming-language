569175e4d6773fe1021de5f74bbfd49f0628ed18
kevin jose <101249625+kevinjoses@users.noreply.github.com>
2024-11-25 16:28:11 +0530

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,16 @@
+class sampleclass:
+    i=5
+    def my_function01(self):
+        print("hello")
+    def my_function02(self):
+        print("world")
+
+obj=sampleclass()
+print(obj.i)
+obj.my_function02()
+
+
+k="kevin"
+x= k.capitalize()
+print (type(k))
+print(x)
